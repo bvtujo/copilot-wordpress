389bd4939f05d1e6940774b0b8bd11a7a8cfbd02
Austin Ely <austiely@amazon.com>
2021-05-13 09:55:54 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 [ -z $WP_SECRET ] && echo "Secret WP_SECRET not populated in environment" && exit 1
 
 export WORDPRESS_DATABASE_HOST=`echo $WP_SECRET | jq -r '.host'`
-export WORDPRESS_DATABASE_PORT_NUMBER=`echo $WP_SECRET | jq -r .port`
-export WORDPRESS_DATABASE_NAME=`echo $WP_SECRET | jq -r .dbname`
-export WORDPRESS_DATABASE_USER=`echo $WP_SECRET | jq -r .username`
-export WORDPRESS_DATABASE_PASSWORD=`echo $WP_SECRET | jq -r .password`
+export WORDPRESS_DATABASE_PORT_NUMBER=`echo $WP_SECRET | jq -r '.port'`
+export WORDPRESS_DATABASE_NAME=`echo $WP_SECRET | jq -r '.dbname'`
+export WORDPRESS_DATABASE_USER=`echo $WP_SECRET | jq -r '.username'`
+export WORDPRESS_DATABASE_PASSWORD=`echo $WP_SECRET | jq -r '.password'`
 
 /opt/bitnami/scripts/wordpress/entrypoint.sh /opt/bitnami/scripts/apache/run.sh
